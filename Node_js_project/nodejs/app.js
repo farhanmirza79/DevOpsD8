3ed30ac1a37be0f159d73ef879b5f7a2da41165e
farhanmirza79 <farhanmirzabaig@gmail.com>
2024-12-05 09:19:42 -0500

    adding node js project

---
+++ Node_js_project/nodejs/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5002;
+
+app.get('/', (req, res) => {
+	res.send('Hello, World!! This is a node.js app running on port 5002');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+}); 
